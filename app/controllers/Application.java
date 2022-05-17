d43b5e1efb7bb26afd1153dcc60750cf542d8551
hrosej729 <hrosej729@gmail.com>
2022-05-17 12:28:17 -0500

    captcha added

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import play.*;
+import play.libs.*;
+import play.cache.*;
 import play.data.validation.Required;
 import play.mvc.*;
 
@@ -9,6 +11,13 @@ import java.util.*;
 import models.*;
 
 public class Application extends Controller {
+
+    public static void captcha(String id) {
+        Images.Captcha captcha = Images.captcha();
+        String code = captcha.getText("#E4EAFD");
+        Cache.set(id, code, "10mn");
+        renderBinary(captcha);
+    }
 
     public static void index() {
         Post frontPost = Post.find("order by postedAt desc").first();
@@ -20,6 +29,7 @@ public class Application extends Controller {
 
     public static void show(Long id) {
         Post post = Post.findById(id);
+        String randomID = Codec.UUID();
         render(post);
     }
 
@@ -29,13 +39,23 @@ public class Application extends Controller {
         renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
     }
 
-    public static void postComment(Long postId, @Required String author, @Required String content) {
+    public static void postComment(
+            Long postId,
+            @Required(message="Author is required") String author,
+            @Required(message="A message is required") String content,
+            @Required(message="Please type the code") String code,
+            String randomID)
+    {
         Post post = Post.findById(postId);
-        if (validation.hasErrors()) {
-            render("Application/show.html", post);
+        validation.equals(
+                code, Cache.get(randomID)
+        ).message("Invalid code. Please type it again");
+        if(validation.hasErrors()) {
+            render("Application/show.html", post, randomID);
         }
         post.addComment(author, content);
         flash.success("Thanks for posting %s", author);
+        Cache.delete(randomID);
         show(postId);
     }
 
