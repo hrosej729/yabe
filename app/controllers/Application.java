6fd43b6342a12bca4c3c11e0475a4b955392c394
hrosej729 <hrosej729@gmail.com>
2022-05-23 16:28:23 -0500

    made functional metrics page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,24 +1,18 @@
 package controllers;
-
 import play.*;
 import play.libs.*;
 import play.cache.*;
 import play.data.validation.Required;
 import play.mvc.*;
-
 import java.util.*;
-
 import models.*;
-
 public class Application extends Controller {
-
     public static void captcha(String id) {
         Images.Captcha captcha = Images.captcha();
         String code = captcha.getText("#E4EAFD");
         Cache.set(id, code, "10mn");
         renderBinary(captcha);
     }
-
     public static void index() {
         Post frontPost = Post.find("order by postedAt desc").first();
         List<Post> olderPosts = Post.find(
@@ -26,19 +20,16 @@ public class Application extends Controller {
         ).from(1).fetch(10);
         render(frontPost, olderPosts);
     }
-
     public static void show(Long id) {
         Post post = Post.findById(id);
         String randomID = Codec.UUID();
         render(post);
     }
-
     @Before
     static void addDefaults() {
         renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
         renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
     }
-
     public static void postComment(
             Long postId,
             @Required(message = "Author is required") String author,
@@ -57,7 +48,6 @@ public class Application extends Controller {
         Cache.delete(randomID);
         show(postId);
     }
-
     public static void reactWithLike(
             Long postId,
             @Required(message = "Author is required") String author) {
@@ -66,7 +56,6 @@ public class Application extends Controller {
         flash.success("Thanks for liking, %s!", author);
         show(postId);
     }
-
     public static void listTagged(String tag) {
         List<Post> posts = Post.findTaggedWith(tag);
         render(tag, posts);
@@ -78,4 +67,18 @@ public class Application extends Controller {
         render(creator, comments, posts, likes);
     }
 
+    public static void dataDump(){
+        Map<String, Integer> metrics = new HashMap<String, Integer>();
+
+        List<Post> posts = Post.findAll();
+        List<Comment> comments = Comment.findAll();
+        List<ReactLike> likes = ReactLike.findAll();
+
+        metrics.put("Users", 2);
+        metrics.put("Posts", posts.size());
+        metrics.put("Likes", likes.size());
+        metrics.put("Comments", comments.size());
+
+        renderJSON(metrics);
+    }
 }
